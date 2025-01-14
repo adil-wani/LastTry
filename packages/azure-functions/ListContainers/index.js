e4ba7876503cc26871b95452f9575676937baf04
adil wani <adil.wani@ais.com>
2025-01-14 14:54:31 -0500

    Changed from CommonJS to ES

--- packages/azure-functions/ListContainers/index.js
+++ packages/azure-functions/ListContainers/index.js
@@ -1,74 +1,64 @@
-//const msal = require('@azure/msal-node');
-// const jwt = require('jsonwebtoken');
-// const jwksClient = require('jwks-rsa');
-
+import { ConfidentialClientApplication, LogLevel } from '@azure/msal-node';
 import jwt from 'jsonwebtoken';
 import jwksClient from 'jwks-rsa';
+import 'isomorphic-fetch';
+import { Client as GraphClient } from '@microsoft/microsoft-graph-client';
+import { getGraphToken } from '../utils/auth.js';
 
-// require('isomorphic-fetch'); // required for graph library and SharePoint calls
-// const Graph = require('@microsoft/microsoft-graph-client');
-//const { getGraphToken } = require('../utils/auth.js');
+const DISCOVERY_KEYS_ENDPOINT = process.env["DISCOVERY_KEYS_ENDPOINT"];
+const config = {
+    auth: {
+        clientId: process.env["APP_CLIENT_ID"],
+        authority: process.env["APP_AUTHORITY"],
+        audience: process.env["APP_AUDIENCE"],
+        clientSecret: process.env["APP_CLIENT_SECRET"]
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback(loglevel, message, containsPii) {
+                console.log(message);
+            },
+            piiLoggingEnabled: false,
+            logLevel: LogLevel.Verbose,
+        }
+    }
+};
+const cca = new ConfidentialClientApplication(config);
 
-// const { getGraphToken } = require('../utils/auth.js');
+const isJwtValid = (token) => {
+    if (!token) {
+        return false;
+    }
+    const validationOptions = {
+        algorithms: ['RS256'],
+        audience: config.auth.audience,
+        issuer: config.auth.issuer
+    };
+    jwt.verify(token, getSigningKeys, validationOptions, (err, payload) => {
+        if (err) {
+            console.log(err);
+            return false;
+        }
+        return true;
+    });
+};
 
+const getSigningKeys = (header, callback) => {
+    const client = jwksClient({
+        jwksUri: DISCOVERY_KEYS_ENDPOINT
+    });
 
+    client.getSigningKey(header.kid, (err, key) => {
+        const signingKey = key.publicKey || key.rsaPublicKey;
+        console.log('Signing key: ' + signingKey);
+        callback(null, signingKey);
+    });
+};
 
-// const DISCOVERY_KEYS_ENDPOINT = process.env["DISCOVERY_KEYS_ENDPOINT"];
-// const config = {
-//     auth: {
-//         clientId: process.env["APP_CLIENT_ID"],
-//         authority: process.env["APP_AUTHORITY"],
-//         audience: process.env["APP_AUDIENCE"],
-//         clientSecret: process.env["APP_CLIENT_SECRET"]
-//     },
-//     system: {
-//         loggerOptions: {
-//             loggerCallback(loglevel, message, containsPii) {
-//                 console.log(message);
-//             },
-//             piiLoggingEnabled: false,
-//             logLevel: msal.LogLevel.Verbose,
-//         }
-//     }
-// };
-// const cca = new msal.ConfidentialClientApplication(config);
-
-// const isJwtValid = (token) => {
-//     if (!token) {
-//         return false;
-//     }
-//     const validationOptions = {
-//         algorithms: ['RS256'],
-//         audience: config.auth.audience, // v2.0 token
-//         issuer: config.auth.issuer // v2.0 token
-//         // Also verify JWT has the Container.Manage scope 
-//     }
-//     jwt.verify(token, getSigningKeys, validationOptions, (err, payload) => {
-//         if (err) {
-//             console.log(err);
-//             return false;
-//         }
-//         return true;
-//     });
-// }
-
-// const getSigningKeys = (header, callback) => {
-//     var client = jwksClient({
-//         jwksUri: DISCOVERY_KEYS_ENDPOINT
-//     });
-
-//     client.getSigningKey(header.kid, function (err, key) {
-//         var signingKey = key.publicKey || key.rsaPublicKey;
-//         console.log('Signing key: ' + signingKey);
-//         callback(null, signingKey);
-//     });
-// }
-
-module.exports = async function (context, req) {
+export async function run(context, req) {
     try {
         context.log.info('Function execution started.');
 
-        // Check if the Authorization header is present
         if (!req.headers.authorization) {
             context.log.error('No access token provided.');
             context.res = {
@@ -78,62 +68,43 @@ module.exports = async function (context, req) {
             return;
         }
 
-        // Extract bearer token
         const [bearer, token] = req.headers.authorization.split(' ');
         context.log.info(`Token received: ${token}`);
 
-        // Get Graph Token
         context.log.info('Retrieving Graph token...');
 
-        context.res = {
-            status: 200,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: {
-                value: [{displayName :"Came from ListContainers", id: 1}]
-            }
+        const [graphSuccess, graphTokenResponse] = await getGraphToken(cca, token);
+
+        if (!graphSuccess) {
+            context.log.error('Failed to retrieve Graph token.');
+            context.res = graphTokenResponse;
+            return;
+        }
+
+        context.log.info('Initializing Graph client...');
+        const authProvider = (callback) => {
+            callback(null, graphTokenResponse);
         };
 
+        const options = {
+            authProvider,
+            defaultVersion: 'beta'
+        };
 
-        // const [graphSuccess, graphTokenResponse] = await getGraphToken(cca, token);
+        context.log.info('Calling Microsoft Graph API...');
+        const graph = GraphClient.init(options);
+        const res = await graph
+            .api(`storage/fileStorage/containers?$filter=containerTypeId eq ${process.env["APP_CONTAINER_TYPE_ID"]}`)
+            .get();
 
-        // if (!graphSuccess) {
-        //     context.log.error('Failed to retrieve Graph token.');
-        //     context.res = graphTokenResponse;
-        //     return;
-        // }
+        context.log.info('Graph API call successful.');
 
-        // // Set up Graph client options
-        // context.log.info('Initializing Graph client...');
-        // const authProvider = (callback) => {
-        //     callback(null, graphTokenResponse);
-        // };
-
-        // let options = {
-        //     authProvider,
-        //     defaultVersion: 'beta'
-        // };
-
-        // // Call Microsoft Graph API
-        // context.log.info('Calling Microsoft Graph API...');
-        // const graph = Graph.Client.init(options);
-        // const res = await graph
-        //     .api(`storage/fileStorage/containers?$filter=containerTypeId eq ${process.env["APP_CONTAINER_TYPE_ID"]}`)
-        //     .get();
-
-        // // Log success
-        // context.log.info('Graph API call successful.');
-
-        // // Return successful response
-        // context.res = {
-        //     body: res
-        // };
+        context.res = {
+            body: res
+        };
     } catch (error) {
-        // Log the error in detail
         context.log.error('An error occurred:', error);
 
-        // Return the error to the consumer
         context.res = {
             status: 200,
             body: {
@@ -142,5 +113,4 @@ module.exports = async function (context, req) {
             }
         };
     }
-};
-
+}

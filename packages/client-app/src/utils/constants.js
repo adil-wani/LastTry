db55217cc205e1e3d9944c498229a1b113cce541
adil wani <adil.wani@ais.com>
2025-01-13 20:19:46 -0500

    Removed Localhost 7071

--- packages/client-app/src/utils/constants.js
+++ packages/client-app/src/utils/constants.js
@@ -1,4 +1,5 @@
 export const GRAPH_SCOPE_USER_READ = 'User.Read';
 export const GRAPH_SCOPE_FILES_READ_WRITE_ALL = 'Files.ReadWrite.All';
-export const localFunctionHost = 'http://localhost:7071';
+//export const localFunctionHost = 'http://localhost:7071';
+export const localFunctionHost = '';
 export const remoteFunctionHost = '';

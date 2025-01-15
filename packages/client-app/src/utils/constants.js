e153b84ab9f4fb24af3b425023a07750ce0e9064
Adil Wani <93664646+adil-wani@users.noreply.github.com>
2025-01-14 20:07:12 -0500

    Update constants.js

--- packages/client-app/src/utils/constants.js
+++ packages/client-app/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const GRAPH_SCOPE_USER_READ = 'User.Read';
 export const GRAPH_SCOPE_FILES_READ_WRITE_ALL = 'Files.ReadWrite.All';
 //export const localFunctionHost = 'http://localhost:7071';
-export const localFunctionHost = '';
+export const localFunctionHost = 'https://funxcset.azurewebsites.net/';
 export const remoteFunctionHost = '';

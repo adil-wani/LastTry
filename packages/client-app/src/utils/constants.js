8a747530e644b016a7ce8001d6a1814af7379cab
adil wani <adil.wani@ais.com>
2025-01-14 21:28:07 -0500

    MOre changes

--- packages/client-app/src/utils/constants.js
+++ packages/client-app/src/utils/constants.js
@@ -1,5 +1,6 @@
 export const GRAPH_SCOPE_USER_READ = 'User.Read';
 export const GRAPH_SCOPE_FILES_READ_WRITE_ALL = 'Files.ReadWrite.All';
 //export const localFunctionHost = 'http://localhost:7071';
-export const localFunctionHost = 'https://funxcset.azurewebsites.net/';
+//export const localFunctionHost = 'https://funxcset.azurewebsites.net/';
 export const remoteFunctionHost = '';
+export const localFunctionHost = '';
